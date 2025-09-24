6643a54e93f7a23fa5ef0e2fe3cf80bf681802cf
davi2238 <david_groenkjar@hotmail.com>
2025-09-24 08:56:00 +0000

    Ændringen vi vil have

--- test_packages.R
+++ test_packages.R
@@ -1,7 +1,3 @@
 library(pryr)
 library(RPostgres)
-<<<<<<< HEAD
-library(tidyverse) # Often used in data science
-=======
-library(tidyverse) # Ændring i remote til konflikt test
->>>>>>> 5f5922dfbcd0e26d53e867db8c90669a08405198
+library(tidyverse) # Denne linje vi vil beholde
